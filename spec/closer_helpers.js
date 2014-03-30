cbaba878a03f21959a2e0a9377c226e3e72971c3
Vicky Chijwani <vickychijwani@gmail.com>
2014-03-30 20:44:33 +0530

    Add helpers for node construction to spec.

---
+++ spec/closer_helpers.js
@@ -0,0 +1,83 @@
+exports.Literal = function (value) {
+    return {
+        type: "Literal",
+        value: value
+    };
+};
+
+exports.Identifier = function (name) {
+    return {
+        type: "Identifier",
+        name: name
+    };
+};
+
+exports.CallExpression = function (callee, args) {
+    return {
+        type: "CallExpression",
+        callee: callee,
+        arguments: (args !== undefined) ? args : []
+    };
+};
+
+exports.FunctionExpression = function (id, params, rest, body) {
+    return {
+        type: "FunctionExpression",
+        id: id,
+        params: params,
+        defaults: [],
+        rest: rest,
+        body: body,
+        generator: false,
+        expression: false
+    };
+};
+
+exports.EmptyStatement = function () {
+    return {
+        type: "EmptyStatement"
+    };
+};
+
+exports.ExpressionStatement = function (expression) {
+    return {
+        type: "ExpressionStatement",
+        expression: expression
+    };
+};
+
+exports.IfStatement = function (test, consequent, alternate) {
+    return {
+        type: "IfStatement",
+        test: test,
+        consequent: consequent,
+        alternate: (alternate !== undefined) ? alternate : null
+    };
+};
+
+exports.ReturnStatement = function (argument) {
+    return {
+        type: "ReturnStatement",
+        argument: argument
+    };
+};
+
+exports.FunctionDeclaration = function (id, params, rest, body) {
+    var node = exports.FunctionExpression(id, params, rest, body);
+    node.type = "FunctionDeclaration";
+    return node;
+};
+
+exports.BlockStatement = function () {
+    return {
+        type: "BlockStatement",
+        body: Array.prototype.slice.call(arguments)
+    };
+};
+
+exports.Program = function () {
+    return {
+        type: "Program",
+        body: Array.prototype.slice.call(arguments)
+    };
+};
--- spec/closer_spec.js
+++ spec/closer_spec.js
@@ -1,6 +1,19 @@
 var closer = require("../src/closer");
 var json_diff = require("json-diff");
 
+var helpers = require("./closer_helpers");
+var Literal = helpers.Literal;
+var Identifier = helpers.Identifier;
+var CallExpression = helpers.CallExpression;
+var FunctionExpression = helpers.FunctionExpression;
+var EmptyStatement = helpers.EmptyStatement;
+var ExpressionStatement = helpers.ExpressionStatement;
+var IfStatement = helpers.IfStatement;
+var ReturnStatement = helpers.ReturnStatement;
+var FunctionDeclaration = helpers.FunctionDeclaration;
+var BlockStatement = helpers.BlockStatement;
+var Program = helpers.Program;
+
 beforeEach(function () {
     this.addMatchers({
         toDeepEqual: function (expected) {
@@ -18,355 +31,152 @@ describe("Closer.js", function () {
 
     // atoms
     it("correctly parses an empty program", function () {
-        expect(closer.parse("\n")).toDeepEqual({
-            type: 'Program',
-            body: []
-        });
+        expect(closer.parse("\n")).toDeepEqual(Program());
     });
 
     it("correctly parses an empty s-expression", function () {
-        expect(closer.parse("()\n")).toDeepEqual({
-            type: "Program",
-            body: [{
-                type: "EmptyStatement"
-            }]
-        });
+        expect(closer.parse("()\n")).toDeepEqual(Program(
+            EmptyStatement()
+        ));
     });
 
     it("correctly parses an identifier", function () {
-        expect(closer.parse("x\n")).toDeepEqual({
-            type: 'Program',
-            body: [{
-                type: "ExpressionStatement",
-                expression: {
-                    type: "Identifier",
-                    name: "x"
-                }
-            }]
-        });
+        expect(closer.parse("x\n")).toDeepEqual(Program(
+            ExpressionStatement(Identifier("x"))
+        ));
     });
 
     it("correctly parses integer, string, boolean, and nil literals", function () {
-        expect(closer.parse("24\n\"string\"\ntrue\nfalse\nnil\n")).toDeepEqual({
-            type: 'Program',
-            body: [{
-                type: "ExpressionStatement",
-                expression: {
-                    type: "Literal",
-                    value: 24
-                }
-            }, {
-                type: "ExpressionStatement",
-                expression: {
-                    type: "Literal",
-                    value: "string"
-                }
-            }, {
-                type: "ExpressionStatement",
-                expression: {
-                    type: "Literal",
-                    value: true
-                }
-            }, {
-                type: "ExpressionStatement",
-                expression: {
-                    type: "Literal",
-                    value: false
-                }
-            }, {
-                type: "ExpressionStatement",
-                expression: {
-                    type: "Literal",
-                    value: null
-                }
-            }]
-        });
+        expect(closer.parse("24\n\"string\"\ntrue\nfalse\nnil\n")).toDeepEqual(Program(
+            ExpressionStatement(Literal(24)),
+            ExpressionStatement(Literal("string")),
+            ExpressionStatement(Literal(true)),
+            ExpressionStatement(Literal(false)),
+            ExpressionStatement(Literal(null))
+        ));
     });
 
     // functions
     it("correctly parses a function call with 0 arguments", function () {
-        expect(closer.parse("(fn-name)\n")).toDeepEqual({
-            type: "Program",
-            body: [{
-                type: "ExpressionStatement",
-                expression: {
-                    type: "CallExpression",
-                    arguments: [],
-                    callee: {
-                        type: "Identifier",
-                        name: "fn-name"
-                    }
-                }
-            }]
-        });
+        expect(closer.parse("(fn-name)\n")).toDeepEqual(Program(
+            ExpressionStatement(
+                CallExpression(Identifier("fn-name"))
+            )
+        ));
     });
 
     it("correctly parses a function call with > 0 arguments", function () {
-        expect(closer.parse("(fn-name arg1 arg2)\n")).toDeepEqual({
-            type: "Program",
-            body: [{
-                type: "ExpressionStatement",
-                expression: {
-                    type: "CallExpression",
-                    arguments: [{
-                        type: "Identifier",
-                        name: "arg1"
-                    },
-                        {
-                            type: "Identifier",
-                            name: "arg2"
-                        }],
-                    callee: {
-                        type: "Identifier",
-                        name: "fn-name"
-                    }
-                }
-            }]
-        });
+        expect(closer.parse("(fn-name arg1 arg2)\n")).toDeepEqual(Program(
+            ExpressionStatement(
+                CallExpression(
+                    Identifier("fn-name"),
+                    [Identifier("arg1"), Identifier("arg2")]
+                )
+            )
+        ));
     });
 
     it("correctly parses an anonymous function definition", function () {
-        expect(closer.parse("(fn [x] x)\n")).toDeepEqual({
-            type: "Program",
-            body: [{
-                type: "ExpressionStatement",
-                expression: {
-                    type: "FunctionExpression",
-                    id: null,
-                    params: [{
-                        type: "Identifier",
-                        name: "x"
-                    }],
-                    defaults: [],
-                    rest: null,
-                    body: {
-                        type: "BlockStatement",
-                        body: [{
-                            type: "ReturnStatement",
-                            argument: {
-                                type: "Identifier",
-                                name: "x"
-                            }
-                        }]
-                    },
-                    generator: false,
-                    expression: false
-                }
-            }]
-        });
+        expect(closer.parse("(fn [x] x)\n")).toDeepEqual(Program(
+            ExpressionStatement(
+                FunctionExpression(
+                    null,
+                    [Identifier("x")],
+                    null,
+                    BlockStatement(
+                        ReturnStatement(Identifier("x"))
+                    )
+                )
+            )
+        ));
     });
 
     it("correctly parses an anonymous function call", function () {
-        expect(closer.parse("((fn [x] x) 2)\n")).toDeepEqual({
-            type: "Program",
-            body: [{
-                type: "ExpressionStatement",
-                expression: {
-                    type: "CallExpression",
-                    arguments: [{
-                        type: "Literal",
-                        value: 2
-                    }],
-                    callee: {
-                        type: "FunctionExpression",
-                        id: null,
-                        params: [{
-                            type: "Identifier",
-                            name: "x"
-                        }],
-                        defaults: [],
-                        rest: null,
-                        body: {
-                            type: "BlockStatement",
-                            body: [{
-                                type: "ReturnStatement",
-                                argument: {
-                                    type: "Identifier",
-                                    name: "x"
-                                }
-                            }]
-                        },
-                        generator: false,
-                        expression: false
-                    }
-                }
-            }]
-        });
+        expect(closer.parse("((fn [x] x) 2)\n")).toDeepEqual(Program(
+            ExpressionStatement(
+                CallExpression(
+                    FunctionExpression(
+                        null,
+                        [Identifier("x")],
+                        null,
+                        BlockStatement(
+                            ReturnStatement(Identifier("x"))
+                        )
+                    ),
+                    [Literal(2)]
+                )
+            )
+        ));
     });
 
     it("correctly parses a named function definition", function () {
-        expect(closer.parse("(defn fn-name [x] x)\n")).toDeepEqual({
-            type: "Program",
-            body: [{
-                type: "FunctionDeclaration",
-                id: {
-                    type: "Identifier",
-                    name: "fn-name"
-                },
-                params: [{
-                    type: "Identifier",
-                    name: "x"
-                }],
-                defaults: [],
-                rest: null,
-                body: {
-                    type: "BlockStatement",
-                    body: [{
-                        type: "ReturnStatement",
-                        argument: {
-                            type: "Identifier",
-                            name: "x"
-                        }
-                    }]
-                },
-                generator: false,
-                expression: false
-            }]
-        });
+        expect(closer.parse("(defn fn-name [x] x)\n")).toDeepEqual(Program(
+            FunctionDeclaration(
+                Identifier("fn-name"),
+                [Identifier("x")],
+                null,
+                BlockStatement(
+                    ReturnStatement(Identifier("x"))
+                )
+            )
+        ));
     });
 
     it("correctly parses rest arguments", function () {
-        expect(closer.parse("(defn avg [& rest] (/ (apply + rest) (count rest)))\n")).toDeepEqual({
-            type: "Program",
-            body: [{
-                type: "FunctionDeclaration",
-                id: {
-                    type: "Identifier",
-                    name: "avg"
-                },
-                params: [],
-                defaults: [],
-                rest: {
-                    type: "Identifier",
-                    name: "rest"
-                },
-                body: {
-                    type: "BlockStatement",
-                    body: [{
-                        type: "ReturnStatement",
-                        argument: {
-                            type: "CallExpression",
-                            arguments: [{
-                                type: "CallExpression",
-                                arguments: [{
-                                    type: "Identifier",
-                                    name: "+"
-                                }, {
-                                    type: "Identifier",
-                                    name: "rest"
-                                }],
-                                callee: {
-                                    type: "Identifier",
-                                    name: "apply"
-                                }
-                            }, {
-                                type: "CallExpression",
-                                arguments: [{
-                                    type: "Identifier",
-                                    name: "rest"
-                                }],
-                                callee: {
-                                    type: "Identifier",
-                                    name: "count"
-                                }
-                            }],
-                            callee: {
-                                type: "Identifier",
-                                name: "/"
-                            }
-                        }
-                    }]
-                },
-                generator: false,
-                expression: false
-            }]
-        });
+        expect(closer.parse("(defn avg [& rest] (/ (apply + rest) (count rest)))\n")).toDeepEqual(Program(
+            FunctionDeclaration(
+                Identifier("avg"),
+                [],
+                Identifier("rest"),
+                BlockStatement(ReturnStatement(
+                    CallExpression(
+                        Identifier("/"),
+                        [
+                            CallExpression(
+                                Identifier("apply"),
+                                [Identifier("+"), Identifier("rest")]
+                            ),
+                            CallExpression(
+                                Identifier("count"),
+                                [Identifier("rest")]
+                            )
+                        ]
+                    ))
+                )
+            )
+        ));
     });
 
     // conditional statements
     it("correctly parses an if-else statement", function () {
-        expect(closer.parse("(if (>= x 0) x (- x))\n")).toDeepEqual({
-            type: "Program",
-            body: [{
-                type: "IfStatement",
-                test: {
-                    type: "CallExpression",
-                    arguments: [{
-                        type: "Identifier",
-                        name: "x"
-                    }, {
-                        type: "Literal",
-                        value: 0
-                    }],
-                    callee: {
-                        type: "Identifier",
-                        name: ">="
-                    }
-                },
-                consequent: {
-                    type: "ExpressionStatement",
-                    expression: {
-                        type: "Identifier",
-                        name: "x"
-                    }
-                },
-                alternate: {
-                    type: "ExpressionStatement",
-                    expression: {
-                        type: "CallExpression",
-                        arguments: [{
-                            type: "Identifier",
-                            name: "x"
-                        }],
-                        callee: {
-                            type: "Identifier",
-                            name: "-"
-                        }
-                    }
-                }
-            }]
-        });
+        expect(closer.parse("(if (>= x 0) x (- x))\n")).toDeepEqual(Program(
+            IfStatement(
+                CallExpression(
+                    Identifier(">="),
+                    [Identifier("x"), Literal(0)]
+                ),
+                ExpressionStatement(Identifier("x")),
+                ExpressionStatement(CallExpression(
+                    Identifier("-"),
+                    [Identifier("x")]
+                ))
+            )
+        ));
     });
 
     it("correctly parses a when form", function () {
-        expect(closer.parse("(when (condition) (println \"hello\") true)\n")).toDeepEqual({
-            type: "Program",
-            body: [{
-                type: "IfStatement",
-                test: {
-                    type: "CallExpression",
-                    arguments: [],
-                    callee: {
-                        type: "Identifier",
-                        name: "condition"
-                    }
-                },
-                consequent: {
-                    type: "BlockStatement",
-                    body: [{
-                        type: "ExpressionStatement",
-                        expression: {
-                            type: "CallExpression",
-                            arguments: [{
-                                type: "Literal",
-                                value: "hello"
-                            }],
-                            callee: {
-                                type: "Identifier",
-                                name: "println"
-                            }
-                        }
-                    }, {
-                        type: "ExpressionStatement",
-                        expression: {
-                            type: "Literal",
-                            value: true
-                        }
-                    }]
-                },
-                alternate: null
-            }]
-        });
+        expect(closer.parse("(when (condition?) (println \"hello\") true)\n")).toDeepEqual(Program(
+            IfStatement(
+                CallExpression(Identifier("condition?")),
+                BlockStatement(
+                    ExpressionStatement(CallExpression(
+                        Identifier("println"),
+                        [Literal("hello")]
+                    )),
+                    ExpressionStatement(Literal(true))
+                )
+            )
+        ));
     });
 
     // pending
