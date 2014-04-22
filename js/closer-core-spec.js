21245a984937ed925bb1a1c90a49c30f24cd2047
Vicky Chijwani <vickychijwani@gmail.com>
2014-04-22 17:17:29 +0530

    CORE: add range.

--- js/closer-core-spec.js
+++ js/closer-core-spec.js
@@ -541,6 +541,13 @@
       assert.associative(map);
       return m.find(map, key);
     },
+    'range': function() {
+      var args;
+      args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      assert.arity(0, 3, arguments);
+      assert.numbers(args);
+      return m.range.apply(this, args);
+    },
     'identity': function(x) {
       assert.arity(1, 1, arguments);
       return x;
@@ -2631,6 +2638,19 @@ if (typeof module !== 'undefined' && _dereq_.main === module) {
         throws('(find \'(1 2 3 4) 2)');
         throws('(find #{1 2 3 4} 2)');
         return throws('(find "string" 2)');
+      });
+    });
+    describe('(range), (range end), (range start end), (range start end step)', function() {
+      return it('returns a seq of numbers in the range [start, end), where start defaults to 0, step to 1, and end to infinity', function() {
+        throws('(range 1 10 2 2)');
+        eq('(nth (range) 23)', 23);
+        eq('(range 5)', seq([0, 1, 2, 3, 4]));
+        eq('(range 2 5)', seq([2, 3, 4]));
+        eq('(range 1 10 2)', seq([1, 3, 5, 7, 9]));
+        eq('(range 10 2 2)', emptySeq());
+        eq('(range 10 2 -2)', seq([10, 8, 6, 4]));
+        eq('(range 1.2 6.9 1.6)', seq([1.2, 2.8, 4.4, 6.0]));
+        return throws('(range 1.2 6.9 "1.6")');
       });
     });
     describe('(identity x)', function() {
